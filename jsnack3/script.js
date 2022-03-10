51ff10204421abe54e5ec4f8a425f8698454af32
Cristian Tulbu <cristian.tulbu@gmail.com>
2022-03-10 16:47:08 +0100

    jsnack3

---
+++ jsnack3/script.js
@@ -0,0 +1,21 @@
+
+
+let total = 0;
+
+let number = parseInt(prompt('Inserisci un numero'));
+let str = number.toString();
+while (Number.isNaN(number) || str.length !== 4) {
+    alert('inserire un numero di 4 cifre');
+    number = parseInt(prompt('Inserisci un numero'));
+    str = number.toString();
+}
+
+
+for (i = 0; i < str.length; i++) {
+    total = total + parseInt(str.charAt(i));
+}
+
+console.log('il numero da te selezionato è', str);
+console.log('la somma è', total);
+
+
